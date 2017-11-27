c9f40ac3d7f316c5eb2d0cafca1f45e8b912bc03
tenchi4u <34038701+tenchi4u@users.noreply.github.com>
2017-11-27 12:18:28 -0600

    Add files via upload

---
+++ Windows_postload.ps1
@@ -0,0 +1,4 @@
+﻿start-process 'c:\temp\localSCCMinstall_PROD.bat' -Wait
+start-process 'c:\temp\localSCOMinstall_PROD.bat' -Wait
+start-process 'c:\temp\SDDC_PLP.ps1' -Wait
+Remove-Item c:\temp\* -Recurse -Force
